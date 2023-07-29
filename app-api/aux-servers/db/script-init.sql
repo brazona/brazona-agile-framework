56883609ea6067acbc0a38b8a4daec745395a1e5
Cezar Silva <cezarfelipedasilva@gmail.com>
2023-07-28 23:24:05 -0300

    feat: connect eureka, api-gateway and config server

--- app-api/aux-servers/db/script-init.sql
+++ app-api/aux-servers/db/script-init.sql
@@ -1,7 +1,7 @@
--- Table: roles
+-- 1. SCHEMA
 
--- DROP TABLE IF EXISTS roles;
-
+-- ROLES
+DROP TABLE IF EXISTS roles;
 CREATE TABLE IF NOT EXISTS roles
 (
     id bigint NOT NULL,
@@ -12,16 +12,10 @@ CREATE TABLE IF NOT EXISTS roles
     CONSTRAINT roles_pkey PRIMARY KEY (id),
     CONSTRAINT uk_ofx66keruapi6vyqpv6f2or37 UNIQUE (name)
 )
-
 TABLESPACE pg_default;
 
-ALTER TABLE IF EXISTS roles
-    OWNER to develop;
-
--- Table: users
-
--- DROP TABLE IF EXISTS users;
-
+-- USERS
+DROP TABLE IF EXISTS users;
 CREATE TABLE IF NOT EXISTS users
 (
     id bigint NOT NULL,
@@ -34,17 +28,10 @@ CREATE TABLE IF NOT EXISTS users
     password character varying(255) COLLATE pg_catalog."default",
     CONSTRAINT users_pkey PRIMARY KEY (id)
 )
-
 TABLESPACE pg_default;
-
-ALTER TABLE IF EXISTS users
-    OWNER to develop;
-
--- Table: tb_user_role
-
--- DROP TABLE IF EXISTS tb_user_role;
-
-CREATE TABLE IF NOT EXISTS tb_user_role
+-- USER_ROLES
+DROP TABLE IF EXISTS user_role;
+CREATE TABLE IF NOT EXISTS user_role
 (
     user_id bigint NOT NULL,
     role_id bigint NOT NULL,
@@ -57,21 +44,41 @@ CREATE TABLE IF NOT EXISTS tb_user_role
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
 )
-
 TABLESPACE pg_default;
+-- PROPERTIES
+DROP TABLE IF EXISTS properties;
+CREATE TABLE properties (
+  id INT AUTO_INCREMENT  PRIMARY KEY,
+  APPLICATION VARCHAR(25) NOT NULL,
+  PROFILE VARCHAR(25) NOT NULL,
+  LABEL VARCHAR(25) DEFAULT NULL,
+  KEY VARCHAR(25) NOT NULL,
+  VALUE VARCHAR(200) NOT NULL
+);
+-- 2. DATA
 
-ALTER TABLE IF EXISTS tb_user_role
-    OWNER to develop;
-
-
+-- USERS
 INSERT INTO users(id, active, created, is_account_non_expired, is_credentials_non_expired, login, password)
-VALUES (1, true, '2023/07/18', true, true,'nina@gmail.com', '$2a$12$G1UdbxJtZMTvp95oMiNXkOpCA.2RtDbz0FrLQDQMWPEQjtvZbcc3C');
+VALUES (1, true, '2023/07/18', true, true,'cezar@gmail.com', '$2a$12$G1UdbxJtZMTvp95oMiNXkOpCA.2RtDbz0FrLQDQMWPEQjtvZbcc3C');
 INSERT INTO users(id, active, created, is_account_non_expired, is_credentials_non_expired, login, password)
-VALUES (2, true, '2023/07/18', true, true,'leia@gmail.com', '$2a$12$G1UdbxJtZMTvp95oMiNXkOpCA.2RtDbz0FrLQDQMWPEQjtvZbcc3C');
-
+VALUES (2, true, '2023/07/18', true, true,'jadina@gmail.com', '$2a$12$G1UdbxJtZMTvp95oMiNXkOpCA.2RtDbz0FrLQDQMWPEQjtvZbcc3C');
+-- ROLES
 INSERT INTO roles (id,name) VALUES (1,'ROLE_OPERATOR');
 INSERT INTO roles (id,name) VALUES (2,'ROLE_ADMIN');
-
-INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
-INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
-INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+-- USER_ROLES
+INSERT INTO user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO user_role (user_id, role_id) VALUES (2, 1);
+INSERT INTO user_role (user_id, role_id) VALUES (2, 2);
+-- PROPERTIES
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('myapp', 'MYCLIENT', '1.1', 'prop1', 'val11');
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('myapp', 'MYCLIENT', '1.1', 'prop2', 'val12');
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('myapp', 'MYCLIENT', '1.1', 'prop3', 'val13');
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('myapp', 'MYCLIENT', '1.0', 'prop1', 'val21');
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('myapp', 'MYCLIENT', '1.0', 'prop2', 'val22');
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('myapp', 'MYCLIENT', '1.0', 'prop3', 'val23');
---
+++ app-api/config-server/src/main/resources/data.sql
@@ -0,0 +1,33 @@
+-- DEFAUlT
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('default', 'dev', 'latest', 'oauth.client.name', 'myappname123');
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('default', 'dev', 'latest', 'oauth.client.secret', 'myappsecret123');
+-- EUREKA SERVER
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('eureka-server', 'dev', 'latest', 'spring.application.name', 'eureka-server');
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('eureka-server', 'dev', 'latest', 'server.port', '8761');
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('eureka-server', 'dev', 'latest', 'eureka.client.register-with-eureka', 'false');
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('eureka-server', 'dev', 'latest', 'eureka.client.fetch-registry', 'false');
+-- API GATEWAY
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('api-gateway', 'dev', 'latest', 'spring.application.name', 'api-gateway');
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('api-gateway', 'dev', 'latest', 'server.port', '8765');
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('api-gateway', 'dev', 'latest', 'eureka.client.service-url.defaultZone', 'http://localhost:8761/eureka');
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('api-gateway', 'dev', 'latest', 'zuul.routes.user.service-id', 'users');
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('api-gateway', 'dev', 'latest', 'zuul.routes.user.path', '/users/**');
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('api-gateway', 'dev', 'latest', 'hystrix.command.default.execution.isolation.thread.timeoutInMilliseconds', 60000);
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('api-gateway', 'dev', 'latest', 'ribbon.ConnectTimeout', 10000);
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('api-gateway', 'dev', 'latest', 'ribbon.ReadTimeout', 20000);
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('api-gateway', 'dev', 'latest', 'jwt.secret', 'MY-JWT-SECRET');
