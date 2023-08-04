4145ea306d2239ad03f1ee220e2eba1f3bfddb71
Cezar Silva <cezarfelipedasilva@gmail.com>
2023-08-03 23:46:57 -0300

    feat: implement connect security with cert ssl

--- app-api/config-server/src/main/resources/data.sql
+++ app-api/config-server/src/main/resources/data.sql
@@ -12,6 +12,18 @@ INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
 values('eureka-server', 'dev', 'latest', 'eureka.client.register-with-eureka', 'false');
 INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
 values('eureka-server', 'dev', 'latest', 'eureka.client.fetch-registry', 'false');
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('eureka-server', 'dev', 'latest', 'server.ssl.key-store', 'classpath:br_eureka_app.p12');
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('eureka-server', 'dev', 'latest', 'server.ssl.key-store-password', 'BOsaPRSp6w');
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('eureka-server', 'dev', 'latest', 'server.ssl.key-alias', 'br_eureka_app');
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('eureka-server', 'dev', 'latest', 'server.ssl.key-store-type', 'PKCS12');
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('eureka-server', 'dev', 'latest', 'server.ssl.key-store-provider', 'SUN');
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('api-gateway', 'dev', 'latest', 'server.ssl', 'true');
 -- API GATEWAY
 INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
 values('api-gateway', 'dev', 'latest', 'spring.application.name', 'api-gateway');
@@ -20,7 +32,7 @@ values('api-gateway', 'dev', 'latest', 'server.port', '8765');
 INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
 values('api-gateway', 'dev', 'latest', 'server.servlet.context-path', '/api');
 INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
-values('api-gateway', 'dev', 'latest', 'eureka.client.service-url.defaultZone', 'http://localhost:8761/eureka');
+values('api-gateway', 'dev', 'latest', 'eureka.client.service-url.defaultZone', 'https://localhost:8761/eureka');
 INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
 values('api-gateway', 'dev', 'latest', 'zuul.routes.user.service-id', 'users');
 INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
@@ -31,3 +43,15 @@ INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
 values('api-gateway', 'dev', 'latest', 'ribbon.ConnectTimeout', '10000');
 INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
 values('api-gateway', 'dev', 'latest', 'ribbon.ReadTimeout', '20000');
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('api-gateway', 'dev', 'latest', 'server.ssl.key-store', 'classpath:br_api_gateway_app.p12');
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('api-gateway', 'dev', 'latest', 'server.ssl.key-store-password', 'ZMOt1YLIs9');
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('api-gateway', 'dev', 'latest', 'server.ssl.key-alias', 'br_api_gateway_app');
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('api-gateway', 'dev', 'latest', 'server.ssl.key-store-type', 'PKCS12');
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('api-gateway', 'dev', 'latest', 'server.ssl.key-store-provider', 'SUN');
+INSERT INTO PROPERTIES( APPLICATION, PROFILE, LABEL, KEY, VALUE)
+values('api-gateway', 'dev', 'latest', 'server.ssl', 'true');
